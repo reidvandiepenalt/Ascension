9a233e3876a35d3e4512064bc7556c05a857e286
Reid VanDiepen <reidvandie@gmail.com>
2021-07-19 10:08:22 -0400

    started on mummy slope collision

--- Assets/Scripts/MummyScript.cs
+++ Assets/Scripts/MummyScript.cs
@@ -26,12 +26,26 @@ public class MummyScript : MonoBehaviour, IEnemy
     public float distFromEdge = 3;
     public float speed = 10;
     public float maxJumpRange = 25f;
-    public float maxJumpAngleDeg = 40f;
+    public float maxJumpAngleDeg;
     public float jumpDistToTime = 1f;
     private float yOffset;
     public GameObject enemyGFX;
     public float aggroRange;
     private bool doFindPlatform = false;
+
+
+    int horizontalRayCount;
+    int verticalRayCount;
+
+    float maxClimbAngle = 60;
+    float maxDescendAngle = 60;
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+    RaycastOrigins raycastOrigins;
+    private CollisionInfo collisions;
+    const float skinWidth = 0.01f;
+    const float distBetweenRays = 0.2f;
 
     public int Health { get; set; }
     public int MaxHealth { get; set; }
@@ -52,6 +66,7 @@ public class MummyScript : MonoBehaviour, IEnemy
 
     void Start()
     {
+
         Health = MaxHealth;
         filter = new ContactFilter2D();
         filter.SetLayerMask(groundLayer);
@@ -60,11 +75,14 @@ public class MummyScript : MonoBehaviour, IEnemy
         collider = GetComponent<Collider2D>();
         yOffset = rb.centerOfMass.y;
 
+        CalculateRaySpacing();
+
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 10f, groundLayer);
         if (hit)
         {
             target = new Vector2((player.position.x < transform.position.x)?hit.collider.bounds.min.x + 0.5f:
                 hit.collider.bounds.max.x - 0.5f, transform.position.y);
+            collisions.platform = hit.collider;
         }
 
         InvokeRepeating("CheckDist", 0.5f, 0.25f);
@@ -79,19 +97,22 @@ public class MummyScript : MonoBehaviour, IEnemy
             case State.jumping:
                 //check if landed
                 RaycastHit2D leftHit = Physics2D.Raycast(new Vector2(collider.bounds.min.x, collider.bounds.min.y),
-                    Vector2.down, 0.1f, groundLayer);
+                Vector2.down, 0.1f, groundLayer);
                 RaycastHit2D rightHit = Physics2D.Raycast(new Vector2(collider.bounds.max.x, collider.bounds.min.y),
                     Vector2.down, 0.1f, groundLayer);
                 if (leftHit)
                 {
                     state = State.walking;
-                    target = new Vector2((player.transform.position.x < transform.position.x) 
+                    target = new Vector2((player.transform.position.x < transform.position.x)
                         ? leftHit.collider.bounds.min.x + 0.5f : leftHit.collider.bounds.max.x - 0.5f, transform.position.y);
-                } else if (rightHit)
+                    collisions.platform = leftHit.collider;
+                }
+                else if (rightHit)
                 {
                     state = State.walking;
-                    target = new Vector2((player.transform.position.x < transform.position.x) 
+                    target = new Vector2((player.transform.position.x < transform.position.x)
                         ? rightHit.collider.bounds.min.x + 0.5f : rightHit.collider.bounds.max.x - 0.5f, transform.position.y);
+                    collisions.platform = rightHit.collider;
                 }
                 return;
             case State.attacking:
@@ -116,8 +137,6 @@ public class MummyScript : MonoBehaviour, IEnemy
             if(player.position.x < transform.position.x)
             {
                 toMove = Vector2.left * speed * Time.deltaTime;
-                target = new Vector2(Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, groundLayer)
-                    .collider.bounds.min.x + 0.5f, transform.position.y);
                 if((toMove + transform.position).x < target.x)
                 {
                     FindNextPlatform();
@@ -128,24 +147,19 @@ public class MummyScript : MonoBehaviour, IEnemy
             else
             {
                 toMove = Vector2.right * speed * Time.deltaTime;
-                target = new Vector2(Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, groundLayer)
-                    .collider.bounds.max.x - 0.5f, transform.position.y);
                 if ((toMove + transform.position).x > target.x)
                 {
                     FindNextPlatform();
                     return;
                 }
             }
-            transform.position += toMove;
         }
         else
         {
             Vector2 direction = (target.x < transform.position.x) ? Vector2.left : Vector2.right;
             toMove = direction * speed * Time.deltaTime;
-            transform.position += toMove;
         }
         
-
         //update gfx
         if (toMove.x >= 0.01f)
         {
@@ -155,6 +169,8 @@ public class MummyScript : MonoBehaviour, IEnemy
         {
             enemyGFX.transform.localScale = new Vector3(1f, 1f, 1f);
         }
+
+        Move(toMove);
     }
 
     /// <summary>
@@ -174,6 +190,17 @@ public class MummyScript : MonoBehaviour, IEnemy
 
         if(state != State.walking) { return; }
 
+        if(Physics2D.Raycast(player.position, Vector2.down, Mathf.Infinity, groundLayer).collider.bounds.center.Equals(collisions.platform.bounds.center))
+        {
+            target = new Vector2(player.position.x, collisions.platform.bounds.max.y);
+        }
+        else
+        {
+            target = new Vector2((player.transform.position.x < transform.position.x)
+                ? collisions.platform.bounds.min.x + 0.5f : collisions.platform.bounds.max.x - 0.5f, collisions.platform.bounds.max.y);
+        }
+        
+
         //within attack range
         /*/if(Mathf.Abs(transform.position.y - player.position.y) < yRange 
             && Mathf.Abs(transform.position.x - player.position.x) < attackRange)
@@ -182,15 +209,15 @@ public class MummyScript : MonoBehaviour, IEnemy
             Attack();
             return;
         }/*/
-        /*/if(Mathf.Abs(transform.position.y - player.position.y) < yRange
+
+        if(Mathf.Abs(transform.position.y - player.position.y) < yRange
             && Mathf.Abs(transform.position.x - player.position.x) > attackRange)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position,
-                    Vector2.down, yOffset + 0.25f, groundLayer);
-            target = new Vector2((player.transform.position.x < transform.position.x) 
-                ? hit.collider.bounds.min.x + 0.5f : hit.collider.bounds.max.x - 0.5f, transform.position.y);
-        }/*/
-        if (Mathf.Abs(transform.position.y - player.position.y) > yRange)
+            target = new Vector2((player.transform.position.x < transform.position.x)
+                ? collisions.platform.bounds.min.x + 0.5f : collisions.platform.bounds.max.x - 0.5f, transform.position.y);
+        }
+
+        if (Mathf.Abs(transform.position.y - player.position.y) > yRange && Mathf.Abs(transform.position.x - target.x) < distFromEdge)
         {
             doFindPlatform = true;
             return;
@@ -207,7 +234,7 @@ public class MummyScript : MonoBehaviour, IEnemy
 
         float jumpAngleDelta = maxJumpAngleDeg / 5f;
         float angleToPlayer = Vector2.SignedAngle(Vector2.right, player.position - transform.position);
-        for(int i = -5; i <= 5; i++)
+        for(int i = -6; i <= 6; i++)
         {
             float angle = angleToPlayer + (jumpAngleDelta * i);
             Collider2D temp = Physics2D.Raycast(transform.position, new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad),
@@ -260,7 +287,7 @@ public class MummyScript : MonoBehaviour, IEnemy
         Vector2 landingPoint;
         if (slopeTest.distance > platform.bounds.extents.y / 4) 
         {
-            landingPoint = new Vector2(platform.ClosestPoint(transform.position).x, slopeTest.point.y + (yOffset * 2) + 0.25f);
+            landingPoint = new Vector2(platform.ClosestPoint(transform.position).x, slopeTest.point.y + (yOffset * 2) + 0.5f);
         }
         else
         {
@@ -268,9 +295,10 @@ public class MummyScript : MonoBehaviour, IEnemy
         }
         float jumpTime = Mathf.Max((Vector2.Distance(landingPoint, transform.position)) / jumpDistToTime, 0.15f);
         
-        float yVel = Mathf.Clamp((landingPoint.y - collider.bounds.min.y) / jumpTime - ((rb.gravityScale * Physics2D.gravity).y * jumpTime / 2), 10f, 150);
-        float xVel = Mathf.Clamp((landingPoint.x - transform.position.x) / jumpTime, 0, 150);
-        if(xVel == 0) { xVel = (closestPointToPlayer.x - transform.position.x) / jumpTime; }
+        float yVel = Mathf.Clamp((landingPoint.y - collider.bounds.min.y) / jumpTime - ((rb.gravityScale * Physics2D.gravity).y * jumpTime / 2), 10f, 175);
+        float xVel = (landingPoint.x - transform.position.x) / jumpTime;
+        if(xVel < 0) { xVel = Mathf.Clamp(xVel, -100, -5); }
+        else { xVel = Mathf.Clamp(xVel, 5, 100); }
 
         rb.velocity = new Vector2(xVel, yVel);
     }
@@ -279,5 +307,251 @@ public class MummyScript : MonoBehaviour, IEnemy
     {
         //state = State.attacking;
         return null;
+    }
+
+
+    /// <summary>
+    /// Moves the player based on given move distance
+    /// </summary>
+    public void Move(Vector2 moveDistance)
+    {
+        UpdateRaycastOrigins();
+        collisions.Reset();
+        collisions.moveDistanceOld = moveDistance;
+
+        //call collision functions
+        if (moveDistance.y < 0)
+        {
+            DescendSlope(ref moveDistance);
+        }
+        if (moveDistance.x != 0)
+        {
+            HorizontalCollisions(ref moveDistance);
+        }
+        if (moveDistance.y != 0)
+        {
+            VerticalCollisions(ref moveDistance);
+        }
+
+        //move based on collision-modified distance
+        transform.Translate(moveDistance);
+        //update physics engine
+        Physics2D.SyncTransforms();
+    }
+
+    /// <summary>
+    /// Check for collisions in the horizontal direction
+    /// </summary>
+    /// <param name="moveDistance">Direction being moved</param>
+    void HorizontalCollisions(ref Vector2 moveDistance)
+    {
+        float directionX = Mathf.Sign(moveDistance.x);
+        float rayLength = Mathf.Abs(moveDistance.x) + skinWidth;
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, groundLayer);
+
+            if (hit)
+            {
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if (slopeAngle <= maxClimbAngle)
+                {
+                    if (!collisions.below && i != 0)
+                    {
+                        break;
+                    }
+
+                    if (collisions.descendingSlope)
+                    {
+                        collisions.descendingSlope = false;
+                        moveDistance = collisions.moveDistanceOld;
+                    }
+                    float distanceToSlopeStart = 0;
+                    if (slopeAngle != collisions.slopeAngleOld)
+                    {
+                        distanceToSlopeStart = hit.distance - skinWidth;
+                        moveDistance.x -= distanceToSlopeStart * directionX;
+                    }
+                    ClimbSlope(ref moveDistance, slopeAngle);
+                    moveDistance.x += directionX * distanceToSlopeStart;
+                }
+
+                if (!collisions.climbingSlope || slopeAngle > maxClimbAngle)
+                {
+                    moveDistance.x = (hit.distance - skinWidth) * directionX;
+                    rayLength = hit.distance;
+
+                    if (collisions.climbingSlope)
+                    {
+                        moveDistance.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(moveDistance.x);
+                    }
+
+                    collisions.left = directionX == -1;
+                    collisions.right = directionX == 1;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Check for collisions in y axis
+    /// </summary>
+    /// <param name="moveDistance">Current move distance</param>
+    void VerticalCollisions(ref Vector2 moveDistance)
+    {
+        float directionY = Mathf.Sign(moveDistance.y);
+        float rayLength = Mathf.Abs(moveDistance.y) + skinWidth;
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + moveDistance.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, groundLayer);
+
+            if (hit)
+            {
+                moveDistance.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+
+                if (collisions.climbingSlope)
+                {
+                    moveDistance.x = moveDistance.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(moveDistance.x);
+                }
+
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+                collisions.platform = hit.collider;
+            }
+        }
+
+        if (collisions.climbingSlope)
+        {
+            float directionX = Mathf.Sign(moveDistance.x);
+            rayLength = Mathf.Abs(moveDistance.x) + skinWidth;
+            Vector2 rayOrigin = ((directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight) + Vector2.up * moveDistance.y;
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, groundLayer);
+
+            if (hit)
+            {
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if (slopeAngle != collisions.slopeAngle)
+                {
+                    moveDistance.x = (hit.distance - skinWidth) * directionX;
+                    collisions.slopeAngle = slopeAngle;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Climbs up a slope if on one
+    /// </summary>
+    /// <param name="moveDistance">Current move distance</param>
+    /// <param name="slopeAngle">Angle of slope</param>
+    void ClimbSlope(ref Vector2 moveDistance, float slopeAngle)
+    {
+        float moveDistanceDirection = Mathf.Abs(moveDistance.x);
+        float climbmoveDistanceY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistanceDirection;
+        if (moveDistance.y <= climbmoveDistanceY)
+        {
+            moveDistance.y = climbmoveDistanceY;
+            moveDistance.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistanceDirection * Mathf.Sign(moveDistance.x);
+            collisions.below = true;
+            collisions.climbingSlope = true;
+            collisions.slopeAngle = slopeAngle;
+        }
+    }
+
+    /// <summary>
+    /// Smoothly descends a slope if on one
+    /// </summary>
+    /// <param name="moveDistance">Current move distance</param>
+    void DescendSlope(ref Vector2 moveDistance)
+    {
+        float directionX = Mathf.Sign(moveDistance.x);
+        Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity, groundLayer);
+
+        if (hit)
+        {
+            float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+            if (slopeAngle != 0 && slopeAngle <= maxDescendAngle)
+            {
+                if (Mathf.Sign(hit.normal.x) == directionX)
+                {
+                    if (hit.distance - skinWidth <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(moveDistance.x))
+                    {
+                        float moveDistanceDirection = Mathf.Abs(moveDistance.x);
+                        float descendmoveDistanceY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistanceDirection;
+                        moveDistance.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistanceDirection * Mathf.Sign(moveDistance.x);
+                        moveDistance.y -= descendmoveDistanceY;
+
+                        collisions.slopeAngle = slopeAngle;
+                        collisions.descendingSlope = true;
+                        collisions.below = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Update raycasts to current position
+    /// </summary>
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    /// <summary>
+    /// Calculate spacing between collision detection rays
+    /// </summary>
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        float boundsWidth = bounds.size.x;
+        float boundsHeight = bounds.size.y;
+
+        horizontalRayCount = Mathf.RoundToInt(boundsHeight / distBetweenRays);
+        verticalRayCount = Mathf.RoundToInt(boundsWidth / distBetweenRays);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    /// <summary>
+    /// Structure to hold raycast origins
+    /// </summary>
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight, bottomLeft, bottomRight;
+    }
+
+    /// <summary>
+    /// Holds information about current collisions
+    /// </summary>
+    private struct CollisionInfo
+    {
+        public bool above, below, left, right, climbingSlope, descendingSlope;
+        public float slopeAngle, slopeAngleOld;
+        public Vector2 moveDistanceOld;
+        public Collider2D platform;
+
+        public void Reset()
+        {
+            above = below = left = right = climbingSlope = descendingSlope = false;
+            slopeAngleOld = slopeAngle;
+            slopeAngle = 0;
+        }
     }
 }
