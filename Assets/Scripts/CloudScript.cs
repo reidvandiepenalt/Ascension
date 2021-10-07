c0e907d42502dd167610fa5a18bbf52503fa62ae
Reid VanDiepen <reidvandie@gmail.com>
2021-10-06 20:57:26 -0400

    added god rays and some script fixes

--- Assets/Scripts/CloudScript.cs
+++ Assets/Scripts/CloudScript.cs
@@ -8,23 +8,25 @@ public class CloudScript : MonoBehaviour
     [SerializeField] Camera camera;
 
     float camWidth;
+    float spriteWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         camWidth = camera.orthographicSize;
+        spriteWidth = gameObject.GetComponent<SpriteRenderer>().bounds.extents.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.Translate(moveSpeed * Time.fixedDeltaTime, 0, 0);
-        if(transform.position.x > camWidth)
+        if(transform.position.x - spriteWidth > camWidth)
         {
-            transform.position = new Vector3(-camWidth, transform.position.y, 0);
-        }else if (transform.position.x < -camWidth)
+            transform.position = new Vector3(-camWidth - spriteWidth, transform.position.y, 0);
+        }else if (transform.position.x + spriteWidth < -camWidth)
         {
-            transform.position = new Vector3(camWidth, transform.position.y, 0);
+            transform.position = new Vector3(camWidth + spriteWidth, transform.position.y, 0);
         }
     }
 }
