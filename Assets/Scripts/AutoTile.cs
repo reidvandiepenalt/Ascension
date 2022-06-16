bf985bb799a286b970d0f10a119641831b8d2928
Reid VanDiepen <reidvandie@gmail.com>
2022-06-16 16:25:45 -0400

    got rectangular art setup for egypt

--- Assets/Scripts/AutoTile.cs
+++ Assets/Scripts/AutoTile.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class AutoTile : MonoBehaviour
 {
-    [SerializeField] private float tileX = 1;
-    [SerializeField] private float tileY = 1;
+    [SerializeField] private float multiplierX = 1;
+    [SerializeField] private float multiplierY = 1;
+    [SerializeField] bool overrideScale = false;
     Mesh mesh;
     Renderer Rend;
     private Material matCopy;
-    public void Start()
+    public void OnValidate()
     {
         //get the renderer and scale the texture to size of transform
         Rend = GetComponent<Renderer>();
         matCopy = new Material(Rend.material);
         Rend.sharedMaterial = matCopy;
         mesh = GetComponent<MeshFilter>().mesh;
-        tileX = transform.localScale.x;
-        tileY = transform.localScale.y;
+        float tileX = transform.localScale.x * multiplierX;
+        float tileY = transform.localScale.y * multiplierY;
         Rend.material.mainTextureScale = new Vector2(tileX, tileY);
     }
 }
